83d956bf2cf27917fc0c4bf7130e002248d35038
soila.torome <soila.torome@student.moringaschool.com>
2022-10-08 00:08:49 +0300

    Remove the id row from the table

--- src/Components/books/books.js
+++ src/Components/books/books.js
@@ -3,7 +3,6 @@ import React from "react";
 function Books({books}) {
     return (
         <tr>
-            <td>{books.id}</td>
             <td>{books.title}</td>
             <td>{books.genre}</td>
             <td>{books.description}</td>
