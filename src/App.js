cc984625c6ed9a0d36ba437bb595bf915584fcf5
soila.torome <soila.torome@student.moringaschool.com>
2022-10-07 20:00:03 +0300

    Import the Books container component into the app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import BooksContainer from './Components/books-container/BooksContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='align-items-center'>
+        <h3>
+          WRITERS' HUB
+          <small className="text-muted"> a community for all writers</small>
+        </h3>
+      </div>
+      <BooksContainer />
     </div>
   );
 }
