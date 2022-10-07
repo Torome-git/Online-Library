87bbed893ef71bc39144882b04240e3c9c315b50
soila.torome <soila.torome@student.moringaschool.com>
2022-10-07 20:03:35 +0300

    Add a book idea list component

---
+++ src/Components/book-idea-list/BookIdeaList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Books from "../books/books";
+
+function BookIdeaList({allTransactions}) {
+    return (
+    <table className="table">
+        <tbody>
+            <tr>
+                <th scope="row">
+                    <h3>Title</h3>
+                </th>
+                <th scope="row">
+                     <h3>Genre</h3>
+                </th>
+                <th scope="row">
+                    <h3>Description</h3> 
+                </th>
+            </tr>
+            {allTransactions.map((books) => (
+                <Books
+                key={books.id}
+                books={books} 
+                />
+            ))}
+  </tbody>
+</table>
+    )
+}
+
+export default BookIdeaList;
